7c397f0d841039cc3d91fb2724fc6d087d01a22d
Annolla Tulasi <tulasi.tls.111@gmail.com>
2025-06-03 04:38:09 +0530

    terraform module usage

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "vpc module test"
+    bucket = "vpc-tf-remote-state"
     key    = "module_tests" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "module-lock"
